235d77f2bc332996c5476aeca0190e9c02b4956d
dbapura <dbapura@iu.edu>
2026-04-08 16:38:21 -0400

    fix cross-origin session cookie

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -78,8 +78,8 @@ app.use(
     cookie: {
       maxAge: 30 * 24 * 60 * 60 * 1000,
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: true,
+      sameSite: "none",
     },
   })
 );
